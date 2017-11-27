53096c841c5d0dddae39bd14972b85ade39053f8
Dominika Jusińska <dominika.jusinska@gmail.com>
2017-11-27 19:27:15 +0100

    zadania 7

--- lab7/py7_4.py
+++ lab7/py7_4.py
@@ -4,17 +4,21 @@ class MyList(object):
     def __repr__(self):
         return str(self.container)
     def add(self, element):
-        try:
-            self.container.append(element)
-        except NameError as e:
-            raise(e)
-
+        self.container.append(element)
     def remove(self, element):
-        self.container.remove(element)
+        try:
+            self.container.remove(element)
+        except ValueError as e:
+            raise e
     def sort(self):
         self.container.sort()
     def __getitem__(self, index):
-        return self.container[index]
+        to_return = None
+        try:
+            to_return = self.container[index]
+        except IndexError as e:
+            raise e
+        return to_return
 
 
 
@@ -25,6 +29,5 @@ ml.add(3)
 ml.add(0)
 ml.remove(3)
 ml.sort()
-print ml
 print ml[1]
-ml.add(asdf  )
+print ml
